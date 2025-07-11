6eed518fa2d4f1d0cc0d9822eb2c1ef2054572ff
Prilepskiy_AE <woolf7754286@yandex.ru>
2025-07-12 04:24:33 +0700

    impl retrofit

---
+++ data/src/main/java/com/prilepskiy/data/network/SmartResponse.kt
@@ -0,0 +1,19 @@
+package com.prilepskiy.data.network
+
+import com.google.gson.annotations.SerializedName
+
+data class SmartResponse <T>(
+    @SerializedName("results")
+    val result: List<T>,
+    @SerializedName("info")
+    val info:Info
+){
+    data class Info(
+        @SerializedName("pages")
+        val pages:Int,
+        @SerializedName("next")
+        val next:String,
+        @SerializedName("prev")
+        val prev:String
+    )
+}
---
+++ data/src/main/java/com/prilepskiy/data/network/service/RikService.kt
@@ -0,0 +1,26 @@
+package com.prilepskiy.data.network.service
+
+import com.prilepskiy.data.network.SmartResponse
+import com.prilepskiy.data.network.model.CharacnedResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RikService {
+    @GET("api/character")
+    fun getAllCharacters(
+        @Query("page") page: Int
+    ): SmartResponse<CharacnedResponse>
+
+    @GET("api/character")
+    fun getCharactersFilter(
+        @Query("name") name: String,
+        @Query("status") status: String,
+        @Query("type") type: String,
+        @Query("genger") gender: String,
+        @Query("page") page: Int
+    ): SmartResponse<CharacnedResponse>
+
+    @GET("api/character{id}")
+    fun getCharacnedDetail(@Path("id") id: Int): CharacnedResponse
+}
